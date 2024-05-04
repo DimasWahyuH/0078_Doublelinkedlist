41b5c8138d7977c6e106a5dad63258fa6ce6b4c8
DimasWahyuH <dimaswh19@gmail.com>
2024-05-04 21:52:17 +0700

    Menambahkan else dalam if

--- Double Linked List/Double Linked List.cpp
+++ Double Linked List/Double Linked List.cpp
@@ -164,6 +164,7 @@ void revtraverse()
         }
     }
 }
+
 void searchData()
 {
     if (listEmpty() == true)
@@ -177,3 +178,11 @@ void searchData()
     cin >> num;
     if (search(num, &prev, &curr) == false)
         cout << "\nRecord not found" << endl;
+    else
+    {
+        cout << "\nRecord found" << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nName: " << curr->name << endl;
+    }
+
+}
